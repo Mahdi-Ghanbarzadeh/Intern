b19f3aa7629b5562984e51052e3abbb7f5ec729e
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2022-07-25 16:01:32 +0430

    first ant design form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,383 @@
-import logo from "./logo.svg";
+import {
+  AutoComplete,
+  Button,
+  Cascader,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Select,
+  Slider,
+  Switch,
+  Rate,
+  Radio,
+} from "antd";
+import React, { useState } from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./App.css";
-import { useEffect, useState } from "react";
+const { Option } = Select;
 
-function App() {
-  const [time, setTime] = useState();
-  useEffect(() => {
-    let timer = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
-    return () => clearInterval(timer);
-  }, []);
+const App = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Success: ", values);
+  };
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="98">+98</Option>
+        <Option value="1">+1</Option>
+      </Select>
+    </Form.Item>
+  );
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="Rial">ريال</Option>
+        <Option value="USD">$</Option>
+      </Select>
+    </Form.Item>
+  );
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(
+        [".com", ".org", ".net"].map((domain) => `${value}${domain}`)
+      );
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{time}</p>
-      </header>
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        initialValues={{
+          residence: ["zhejiang", "hangzhou", "xihu"],
+          prefix: "98",
+        }}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="select"
+          label="Select"
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please select your country!",
+            },
+          ]}
+        >
+          <Select placeholder="Please select a country">
+            <Option value="iran">Iran</Option>
+            <Option value="china">China</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[
+            {
+              required: true,
+              message: "Please input your phone number!",
+            },
+          ]}
+        >
+          <Input
+            addonBefore={prefixSelector}
+            style={{
+              width: "100%",
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="donation"
+          label="Donation"
+          rules={[
+            {
+              required: true,
+              message: "Please input donation amount!",
+            },
+          ]}
+        >
+          <InputNumber
+            addonAfter={suffixSelector}
+            style={{
+              width: "100%",
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="intro"
+          label="Intro"
+          rules={[
+            {
+              required: true,
+              message: "Please input Intro",
+            },
+          ]}
+        >
+          <Input.TextArea showCount maxLength={100} />
+        </Form.Item>
+
+        <Form.Item
+          name="gender"
+          label="Gender"
+          rules={[
+            {
+              required: true,
+              message: "Please select gender!",
+            },
+          ]}
+        >
+          <Select placeholder="select your gender">
+            <Option value="male">Male</Option>
+            <Option value="female">Female</Option>
+            <Option value="other">Other</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item name="slider" label="Slider">
+          <Slider
+            marks={{
+              0: "A",
+              20: "B",
+              40: "C",
+              60: "D",
+              80: "E",
+              100: "F",
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="select-multiple"
+          label="Select[multiple]"
+          rules={[
+            {
+              required: true,
+              message: "Please select your favourite colors!",
+              type: "array",
+            },
+          ]}
+        >
+          <Select mode="multiple" placeholder="Please select favourite colors">
+            <Option value="red">Red</Option>
+            <Option value="green">Green</Option>
+            <Option value="blue">Blue</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="InputNumber">
+          <Form.Item name="input-number" noStyle>
+            <InputNumber min={1} max={10} />
+          </Form.Item>
+          <span className="ant-form-text"> machines</span>
+        </Form.Item>
+
+        <Form.Item name="switch" label="Switch" valuePropName="checked">
+          <Switch />
+        </Form.Item>
+
+        <Form.Item name="radio-group" label="Radio.Group">
+          <Radio.Group>
+            <Radio value="a">item 1</Radio>
+            <Radio value="b">item 2</Radio>
+            <Radio value="c">item 3</Radio>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item
+          name="radio-button"
+          label="Radio.Button"
+          rules={[{ required: true, message: "Please pick an item!" }]}
+        >
+          <Radio.Group>
+            <Radio.Button value="a">item 1</Radio.Button>
+            <Radio.Button value="b">item 2</Radio.Button>
+            <Radio.Button value="c">item 3</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item name="checkbox-group" label="Checkbox.Group">
+          <Checkbox.Group>
+            <Row>
+              <Col span={8}>
+                <Checkbox value="A" style={{ lineHeight: "32px" }}>
+                  A
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="B" style={{ lineHeight: "32px" }}>
+                  B
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="C" style={{ lineHeight: "32px" }}>
+                  C
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="D" style={{ lineHeight: "32px" }}>
+                  D
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="E" style={{ lineHeight: "32px" }}>
+                  E
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="F" style={{ lineHeight: "32px" }}>
+                  F
+                </Checkbox>
+              </Col>
+            </Row>
+          </Checkbox.Group>
+        </Form.Item>
+
+        <Form.Item name="rate" label="Rate">
+          <Rate />
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject(new Error("Should accept agreement")),
+            },
+          ]}
+          {...tailFormItemLayout}
+        >
+          <Checkbox>
+            I have read the <a href="">agreement</a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
